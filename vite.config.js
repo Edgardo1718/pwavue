1b4ecb9b8f0c70740159b0ea018e33923cd029f6
Edgardo1718 <97913266+Edgardo1718@users.noreply.github.com>
2023-11-14 15:48:56 -0600

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,17 @@
 import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
+import { GenerateSW } from 'workbox-webpack-plugin'
+import { defineConfig, load } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  publicPath: process.env.NODE_ENV === "development" ? "/vuejs-pwa/" : "",
   plugins: [
     vue(),
   ],
+  configureWebpack: {
+    plugins: [new GenerateSW()]
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
